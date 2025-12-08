ee318e6746677f184e0d8daf70433742123152a0
Adeuan Krisma <krismablackmail@gmail.com>
2025-12-08 16:46:50 -0300

    Initial commit: estrutura mínima tema WordPress

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Xadrez Digital Theme Functions
+ *
+ * @package XadrezDigital
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Configuração do tema
+ */
+function xadrez_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus([
+        'primary' => __('Menu Principal', 'xadrezdigital'),
+    ]);
+}
+add_action('after_setup_theme', 'xadrez_theme_setup');
+
+/**
+ * Enfileira estilos
+ */
+function xadrez_enqueue_assets() {
+    wp_enqueue_style('xadrez-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'xadrez_enqueue_assets');
