d6cfe85042e66c8935374f66b7626497d25fdb43
Adeuan Krisma <krismablackmail@gmail.com>
2025-12-10 17:54:50 -0300

    feat: add woocommerce.php template for full WooCommerce product page support

---
+++ woocommerce.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * WooCommerce Template
+ * 
+ * Este arquivo é usado para todas as páginas do WooCommerce:
+ * - Página de produto único
+ * - Arquivos de produtos (categorias, tags, shop)
+ * - Resultados de busca de produtos
+ * 
+ * @package XadrezDigital
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header(); ?>
+
+<main class="bg-stone-50 py-8">
+    <div class="max-w-7xl mx-auto px-8">
+        <?php woocommerce_content(); ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
